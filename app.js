3d785663fecb7152a96690b1639c4227a2a25b03
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-05-03 18:24:43 +0500

    Redirecting Requests

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
 const PORT = process.env.PORT;
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write(
       '<html><head><title>Enter Your Message</title></head><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>'
     );
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy data");
+    res.statusCode = 302;
+    res.setHeader("Location", "/about");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
