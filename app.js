1b51daa855975153ce419b30cb5477723a6fc6c7
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-05-03 20:40:03 +0500

    Node Module System

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
 require("dotenv").config();
 const PORT = process.env.PORT;
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes.js");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write(
-      '<html><head><title>Enter Your Message</title></head><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>'
-    );
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write(
-    "<html><head><title>MY first Page</title></head><body><h1>Hello from my Node.js Server!</h1></body></html>"
-  );
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.on("error", (err) => {
   console.log(err.message);
