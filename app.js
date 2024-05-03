2023ec12103b1e90b15a434c18dbde49bfb2bcde
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-05-03 17:32:48 +0500

    Routing Requests

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const PORT = process.env.PORT;
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(
-    "URl: ",
-    req.url,
-    "\n Header: ",
-    req.headers,
-    "\n method: ",
-    req.method
-  );
+  const url = req.url;
+  if (url === "/") {
+    res.write(
+      '<html><head><title>Enter Your Message</title></head><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>'
+    );
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write(
     "<html><head><title>MY first Page</title></head><body><h1>Hello from my Node.js Server!</h1></body></html>"
