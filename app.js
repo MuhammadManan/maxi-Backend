583af7840bda61bb953024f5df7425bb3be9009e
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-05-04 14:41:04 +0500

    Returning 404 page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const PORT = process.env.PORT || 2000;
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin.js");
@@ -15,7 +16,9 @@ app.use(shopRoutes);
 
 // Routing if url is not found
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found!</h1>");
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "/views", "/PageNotFound.html"));
 });
 
 // server is running
