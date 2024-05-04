5017ff3d62ed3ef1fe39cc6f89fc7bdba52fbf83
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-05-04 20:28:38 +0500

    Outputting Dynamic Content

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,8 @@ const rootDir = require("../util/path.js");
 const adminData = require("./admin.js");
 
 router.get("/", (req, res, next) => {
-  res.render("shop");
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
