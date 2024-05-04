ccb2acb259fd5178b90460087985f5b34d7096d7
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-05-04 09:40:56 +0500

    Adding Middleware

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); // Allow the request to continue to the next middleware
+});
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  res.write("<h1>Good morning Brother!</h1>");
+});
 const server = http.createServer(app);
 
 server.on("error", (err) => {
