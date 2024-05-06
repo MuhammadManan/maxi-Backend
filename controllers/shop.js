e7b64c4ad9dc72ac71b13c809570e11096eb1ea0
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-05-06 22:13:21 +0500

    Displaying Cart Items on the Cart Page

--- controllers/shop.js
+++ controllers/shop.js
@@ -36,9 +36,23 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    pageTitle: "Your Cart",
-    path: "/cart",
+  Cart.getProducts((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (const product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render("shop/cart", {
+        pageTitle: "Your Cart",
+        path: "/cart",
+        products: cartProducts,
+      });
+    });
   });
 };
 
