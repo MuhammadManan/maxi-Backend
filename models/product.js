99e7704cec6bcb85543e18f1e773bf177b7fff0e
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-05-06 09:16:08 +0500

    Displaying the product

--- models/product.js
+++ models/product.js
@@ -18,7 +18,7 @@ const getProductsFromFile = (cb) => {
   });
 };
 module.exports = class {
-  constructor(title, imageUrl, description, price) {
+  constructor(title, imageUrl, price, description) {
     this.title = title;
     this.imageUrl = imageUrl;
     this.price = price;
