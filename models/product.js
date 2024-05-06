c7265a4d65da9bc382967af13b280c16f353d2d7
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-05-06 21:41:56 +0500

    Deleting Cart when product is deleted

--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const fs = require("fs");
-// const { exit } = require("process");
+const Cart = require("./cart");
 
 const p = path.join(
   path.dirname(require.main.filename),
@@ -61,9 +61,11 @@ module.exports = class Product {
 
   static deleteById(id) {
     getProductsFromFile((products) => {
+      const product = products.find((prod) => prod.id === id);
       const updatedProducts = products.filter((prod) => prod.id !== id);
       fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
         if (!err) {
+          Cart.deleteProduct(id, product.price);
           console.log("product is deleted");
         } else {
           console.log(err);
